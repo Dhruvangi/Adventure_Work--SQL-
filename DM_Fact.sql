559abb50540ca1396f7fed65d7f0ffd63dfc18f8
Dhruvangi Patel <dhruvangi2493@gmail.com>
2022-03-03 17:40:57 -0500

    Add files via upload

---
+++ DM_Fact.sql
@@ -0,0 +1,16 @@
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] --  ,[TaxAmt]
+ 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -3 -- Ensures we always only bring three years of date from extraction.
+ORDER BY
+  OrderDateKey ASC
